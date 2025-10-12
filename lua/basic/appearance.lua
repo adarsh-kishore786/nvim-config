168eb66b4253f74d713e16242a621356ce7fcc4c
adarsh-kishore786 <adarsh.kishore786@gmail.com>
2025-10-12 11:40:34 +0530

    Changed text width and enabled virtual messages

--- lua/basic/appearance.lua
+++ lua/basic/appearance.lua
@@ -1,3 +1,10 @@
 vim.opt.ruler = true
 vim.opt.title = true
 vim.opt.cursorline = true
+vim.opt.textwidth = 80
+vim.opt.wrap = true
+vim.opt.formatoptions = "jcroqlt"
+
+vim.diagnostic.config({
+  virtual_text = true
+})
