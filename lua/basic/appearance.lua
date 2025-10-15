4a0038e6b8a38eb8fe136b2844f819afbd64b32b
adarsh-kishore786 <adarsh.kishore786@gmail.com>
2025-10-15 15:26:38 +0530

    Formatting and line wrapping

--- lua/basic/appearance.lua
+++ lua/basic/appearance.lua
@@ -1,9 +1,11 @@
 vim.opt.ruler = true
 vim.opt.title = true
 vim.opt.cursorline = true
-vim.opt.textwidth = 80
+vim.opt.textwidth = 0
 vim.opt.wrap = true
-vim.opt.formatoptions = "jcroqlt"
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.colorcolumn = "80"
 
 vim.diagnostic.config({
   virtual_text = true
