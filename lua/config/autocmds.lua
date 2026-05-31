bbb9bb22d0ecce78e4265a0434977e48730407ba
adarsh-kishore786 <adarsh.kishore786@gmail.com>
2026-05-31 11:39:40 +0530

    Telescope can configure correct directory

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,3 +9,15 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank { timeout = 200 }
   end
 })
+
+-- cd nvim into the working directory
+-- this causes Telescope to work in the cwd, not pwd
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = "config",
+  callback = function()
+    local arg = vim.fn.argv(0)
+    if type(arg) == "string" and vim.fn.isdirectory(arg) == 1 then
+      vim.cmd.cd(arg)
+    end
+  end
+})
